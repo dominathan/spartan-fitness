29d67016ee8b9bca779d5fa7177c1ffe1fc93727
DKerr <drew.kerr86@gmail.com>
2015-03-31 18:45:52 -0500

    Fixed syntax error.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,4 +3,5 @@ Rails.application.config.middleware.use
   provider :spotify, ENV["SPOTIFY_ID"], ENV["SPOTIFY_KEY"]
   provider :google_oauth2, ENV["GOOGLE_ID"], ENV["GOOGLE_SECRET"]
   provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
+  end
 end
