e0f9c92836a9492b06b71ee9bd11945b6319707c
DKerr <drew.kerr86@gmail.com>
2015-03-31 18:48:34 -0500

    Fixing error.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,12 @@
 Rails.application.config.middleware.use
   provider :facebook, ENV["FB_KEY"], ENV["FB_SECRET"]
+end
+Rails.application.config.middleware.use
   provider :spotify, ENV["SPOTIFY_ID"], ENV["SPOTIFY_KEY"]
+end
+Rails.application.config.middleware.use
   provider :google_oauth2, ENV["GOOGLE_ID"], ENV["GOOGLE_SECRET"]
+end
+Rails.application.config.middleware.use
   provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
-  end
 end
