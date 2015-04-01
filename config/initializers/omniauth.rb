af59b54d670e82615b45bfaa4ce5930610fd0acf
DKerr <drew.kerr86@gmail.com>
2015-03-31 19:26:27 -0500

    Adding facebook scope for omniaut.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV["FB_KEY"], ENV["FB_SECRET"]
+           :scope => 'public_profile, email, user_friends', :display => 'popup'
   provider :spotify, ENV["SPOTIFY_ID"], ENV["SPOTIFY_KEY"]
   provider :google_oauth2, ENV["GOOGLE_ID"], ENV["GOOGLE_SECRET"]
   provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
