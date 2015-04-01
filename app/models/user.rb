64b5dc8d55353f21a601dd5e554ae20f7d32687c
DKerr <drew.kerr86@gmail.com>
2015-04-01 13:54:45 -0500

    User associations defined.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User
   field :provider, type: String
   field :uid, type: String
   field :name, type: String
+  embeds_many :posts
+  embeds_many :workouts
+  embeds_many :routines
+  embeds_many :meals
+  has_many :followers
+  has_many :followings
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
