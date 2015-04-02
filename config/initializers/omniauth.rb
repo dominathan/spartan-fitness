0bc719fb24c95091fb7a9491ccbcf8b96fa70eea
DKerr <drew.kerr86@gmail.com>
2015-04-01 19:07:12 -0500

    Getting errors for omniauth requests.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,21 +1,21 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV["FB_KEY"], ENV["FB_SECRET"]
+  provider :facebook, ENV["FB_KEY"], ENV["FB_SECRET"], :provider_ignores_state => true
     {
       :scope => "public_profile, email, user_friends",
       :display => "popup"
     }
 
-  provider :spotify, ENV["SPOTIFY_ID"], ENV["SPOTIFY_KEY"]
+  provider :spotify, ENV["SPOTIFY_ID"], ENV["SPOTIFY_KEY"], :provider_ignores_state => true
     {
       :scope => "playlist-read-private, playlist-modify-public, playlist-modify-private"
     }
-  provider :google_oauth2, ENV["GOOGLE_ID"], ENV["GOOGLE_SECRET"]
+  provider :google_oauth2, ENV["GOOGLE_ID"], ENV["GOOGLE_SECRET"], :provider_ignores_state => true
     {
       :scope => "email, profile",
       :prompt => "select_account",
       :image_aspect_ratio => "original"
     }
-  provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
+  provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"], :provider_ignores_state => true
     {
       :secure_image_url => 'true',
       :image_size => 'original',
